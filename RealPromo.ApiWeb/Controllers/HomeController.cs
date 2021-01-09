886f6dc755c8b298288d273c1a81f0581e017754
Rullyan Gabriel dos Santos <rullyan.gs@hotmail.com>
2021-01-09 14:18:10 -0300

    add project

---
+++ RealPromo.ApiWeb/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RealPromo.ApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealPromo.ApiWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Promocao()
+        {
+            return View();
+        }
+
+        public ActionResult CadastrarPromocao()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealPromo.ApiWeb/Hubs/PromoHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealPromo.ApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealPromo.ApiWeb.Hubs
+{
+    //RPC
+    public class PromoHub : Hub
+    {
+        /*
+         * Cliente - JS/C#/Java
+         * - Cliente -> Hub(C#) (C# - CadastrarPromocao)
+         * - Hub -> Cliente(JS) (JS - ReceberPromocao)
+         */
+
+        public async Task CadastrarPromocao(Promocao promocao)
+        {
+            /*
+             * Bancon
+             * Queue/Scheduler...
+             * Notificar o usuario (SignalR).
+             */
+
+            await Clients.Caller.SendAsync("CadastradoSucesso");
+            await Clients.Others.SendAsync("ReceberPromocao", promocao);
+        }
+    }
+}
